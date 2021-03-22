4bd2cc86eafdba2aade8e7d83ef2727ace2d6941
hel-leen <79870754+hel-leen@users.noreply.github.com>
2021-03-23 01:20:01 +0900

    Delete project_v2 directory

--- project_v2/pages/save_data.php
+++
@@ -1,7 +0,0 @@
-<?php
-$post_data = json_decode(file_get_contents('php://input'), true); 
-$name = "data_v1/".$post_data['filename'].".txt"; 
-$data = $post_data['filedata'];
-// write the file to disk
-file_put_contents($name, $data);
-?>
